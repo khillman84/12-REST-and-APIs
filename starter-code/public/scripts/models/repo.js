0b1a8e5307d27d659f91b7a079a6dca26d0e833c
Kyle Hillman <khillman84@gmail.com>
2017-02-21 17:02:53 -0800

    finished first half of TODOs

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -6,8 +6,17 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
-
+    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
+    $.ajax({
+	     url: 'https://api.github.com/user/repos',
+	     method: 'GET',
+	     headers: {
+        Authorization: `token ${githubToken}`}
+    })
+    .then(data => {
+      repos.all.push(data);
+    })
+    callback();
   };
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
